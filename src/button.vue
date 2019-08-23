af79574c92b89684326664ee766224b83cf45960
许勇辉 <xuyonghui157@163.com>
2019-08-23 23:00:58 +0800

    引入icon 实现左右两个方向

---
+++ src/button.vue
@@ -0,0 +1,67 @@
+<template>
+
+    <button class="x-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href=`#icon-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+
+
+</template>
+
+<script>
+    export default {
+        props:{
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value){
+                    return !(value !== 'left' && value !== 'right');
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .x-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radiuse);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display:inline-flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .x-button:hover{
+        border-color: var(--bor-color-hover);
+    }
+    .x-button:active{
+        background-color: var(--button-active-bg);
+    }
+    .x-button:focus{
+        outline: none;
+    }
+    .content{
+        order: 2;
+    }
+    .icon{
+        order: 1;
+        margin-right:.3em;
+    }
+
+    .icon-right .content{
+        order: 1;
+    }
+    .icon-right .icon{
+        order: 2;
+        margin-right: 0px;
+        margin-left: .3em;
+    }
+</style>
