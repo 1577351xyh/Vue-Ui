170e941a9c033f4c8b337e0d5214fb1be3e40aba
许勇辉 <xuyonghui157@163.com>
2019-08-25 18:59:43 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -1,7 +1,68 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('x-button',Button);
+Vue.component('x-icon',Icon);
+Vue.component('x-button-group',ButtonGroup);
 new Vue({
     el:"#app",
+    data:{
+        loading1:false
+    }
 });
+import chai from 'chai'
+const expect = chai.expect;
+
+{
+    //构造函数
+    const Constructor = Vue.extend(Button);
+    //实例化对象
+    const button = new Constructor({
+        propsData:{
+            icon:'set'
+        }
+    });
+    // set元素
+    button.$mount('#text');
+    console.log(button)
+    let uesElement = button.$el.querySelector('use');
+    let href = uesElement.getAttribute('xlink:href');
+    // 断言是#icon-set
+    expect(href).to.eq('#icon-set')
+}
+
+{
+    //构造函数
+    const Constructor = Vue.extend(Button);
+    //实例化对象
+    const button = new Constructor({
+        propsData:{
+            icon:'set',
+            loading:true
+        }
+    });
+    // set元素
+    button.$mount();
+    let uesElement = button.$el.querySelector('use');
+    let href = uesElement.getAttribute('xlink:href');
+    // 断言是#icon-loading
+    expect(href).to.eq('#icon-loading')
+}
+
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'set',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div)
+    let svg  = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+}
