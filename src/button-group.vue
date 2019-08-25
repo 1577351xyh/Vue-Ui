51a60d74a00914c68bd39b7e96c839c8d11ddc77
许勇辉 <xuyonghui157@163.com>
2019-08-25 12:56:09 +0800

    button-group組件创建完成

---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="g-button-group">
+    <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+name: ""
+}
+</script>
+
+<style lang="less" scoped>
+.g-button-group{
+    display: flex;
+    vertical-align: middle;
+}
+.g-button-group > .x-button{
+    border-radius: 0px;
+}
+
+.g-button-group :first-child{
+    border-top-left-radius: var(--border-radiuse);
+    border-bottom-left-radius: var(--border-radiuse);
+}
+.g-button-group :last-child{
+    border-top-right-radius: var(--border-radiuse);
+    border-bottom-right-radius: var(--border-radiuse);
+}
+.g-button-group :hover{
+    position: relative;
+    z-index: 1;
+}
+</style>
