223d942843be2f67f52cc85d02c1bb9f993a3b32
许勇辉 <xuyonghui157@163.com>
2019-08-26 22:09:57 +0800

    自动化测试,6个用例通过 1.安装 2.配置 3.npm run test

---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg class="x-icon">
+        <use :xlink:href=`#i-${name}`></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props:['name']
+}
+</script>
+
+<style scoped>
+.x-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
