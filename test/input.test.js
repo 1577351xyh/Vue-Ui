3aabd4e5fff9435ffda8133c72a9e9ecf1ecfe86
许勇辉 <564146958@qq.com>
2019-09-03 23:59:22 +0800

    disabled,value测试用例通过

---
+++ test/input.test.js
@@ -0,0 +1,35 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+    //一个类的行为和属性(他可以干嘛干嘛)
+    it('存在.', () => {
+        expect(Input).to.exist
+    })
+    it('可以接收value', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                value: '1234'
+            }
+        }).$mount()
+        const inputElement = vm.$el.querySelector('input')
+        expect(inputElement.value).to.equal('1234')
+        vm.$destroy()
+    })
+    it('可以接收disabled', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                disabled: true
+            }
+        }).$mount()
+        const inputElement = vm.$el.querySelector('input')
+        expect(inputElement.disabled).to.equal(true)
+        vm.$destroy()
+    })
+})
