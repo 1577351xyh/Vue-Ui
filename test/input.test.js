5d65eb9e6c7544dd05c234619d89247ca5796743
许勇辉 <564146958@qq.com>
2019-09-05 23:05:36 +0800

    input支持双向绑定

--- test/input.test.js
+++ test/input.test.js
@@ -70,9 +70,15 @@ describe('Input', () => {
                 vm.$on(eventName,callback);
                 //触发change事件(模拟了用户操作)
                 let event = new Event(eventName);
+                Object.defineProperty(
+                    event,'target',{
+                        value:{value:'hi'},
+                        enumerable:true
+                    }
+                )
                 let inputElement = vm.$el.querySelector('input')
                 inputElement.dispatchEvent(event);
-                expect(callback).to.have.been.calledWith(event);
+                expect(callback).to.have.been.calledWith('hi');
             });
         });
         // it('支持input事件', () => {
