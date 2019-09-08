8af91c31a57feed6c16b450dbeab6a85d0a11fae
许勇辉 <564146958@qq.com>
2019-09-08 22:24:41 +0800

    toast样式实现(添加plugin js插件板块)

---
+++ src/toast.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="toast">
+    <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+name: ""
+}
+</script>
+
+<style scoped lang="scss">
+    $font-size:14px;
+    $toast-height:40px;
+    $toast-bg:rgba(0,0,0,0.75);
+    .toast{
+        font-size: $font-size;
+        height: $toast-height;
+        background-color: $toast-bg;
+        color: #fff;
+        align-items: center;
+        display: flex;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        position: fixed;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0 rgba(0,0,0,0.5);
+        padding: 0 16px;
+    }
+
+</style>
+
