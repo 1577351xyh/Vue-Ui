79123f7f95b66d1a5250a8ec91e3c573825f72b4
许勇辉 <564146958@qq.com>
2019-09-08 22:34:20 +0800

    toast自动关闭

--- src/toast.vue
+++ src/toast.vue
@@ -1,23 +1,46 @@
 <template>
-<div class="toast">
-    <slot></slot>
-</div>
+    <div class="toast">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
-export default {
-name: ""
-}
+    export default {
+        name: "",
+        props: {
+            autoClose: {
+                type: Boolean,
+                default: true
+            },
+            autoCloseDelay: {
+                type: Number,
+                default: 2
+            }
+        },
+        mounted() {
+            if (this.autoClose) {
+                setTimeout(() => {
+                    this.close()
+                }, this.autoCloseDelay * 1000)
+            }
+        },
+        methods:{
+            close(){
+                this.$el.remove();
+                this.$destroy()
+            }
+        }
+    }
 </script>
 
 <style scoped lang="scss">
     $font-size:14px;
     $toast-height:40px;
     $toast-bg:rgba(0,0,0,0.75);
-    .toast{
-        font-size: $font-size;
-        height: $toast-height;
-        background-color: $toast-bg;
+    .toast {
+        font-size:$font-size;
+        height:$toast-height;
+        background-color:$toast-bg;
         color: #fff;
         align-items: center;
         display: flex;
@@ -26,7 +49,7 @@ name: ""
         transform: translateX(-50%);
         position: fixed;
         border-radius: 4px;
-        box-shadow: 0 0 3px 0 rgba(0,0,0,0.5);
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
         padding: 0 16px;
     }
 
