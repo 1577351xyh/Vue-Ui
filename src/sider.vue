7d52f2fc01a53a63189bbc4be316a6b67f687244
许勇辉 <564146958@qq.com>
2019-09-08 17:21:03 +0800

    layout组件实现

--- src/sider.vue
+++ src/sider.vue
@@ -1,13 +1,37 @@
 <template>
-
+    <transition name="fade">
+    <div class="sider" v-if="visible">
+        <slot></slot>
+        <button @click="visible=false">隐藏</button>
+    </div>
+    </transition>
 </template>
 
 <script>
-export default {
-name: ""
-}
+    export default {
+        name: "GuluSide",
+        data(){
+            return{
+                visible:true
+            }
+        }
+    }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.sider{
+    position: relative;
+    button{
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+}
 
+.fade-enter-active, .fade-leave-active{
+    transition: all .5s;
+}
+.fade-enter, .fade-leave-to{
+    margin-left: -200px;
+}
 </style>
