11a134329557a23d469e05c8ed5563c6da958e4c
许勇辉 <564146958@qq.com>
2019-09-08 16:48:50 +0800

    layout组件基本功能完场

--- src/layout.vue
+++ src/layout.vue
@@ -1,15 +1,37 @@
 <template>
-<div class="layout">
-    <slot></slot>
-</div>
+    <div class="layout" :class="layoutClass">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
-export default {
-name: ""
-}
+    export default {
+        name: "",
+        data() {
+            return {
+                layoutClass:{
+                    hasSider:false
+                }
+            }
+        },
+        mounted() {
+            this.$children.forEach((vm) => {
+                if (vm.$options.name === 'GuluSide') {
+                    this.layoutClass.hasSider = true;
+                }
+            })
+        }
+    }
+
 </script>
 
 <style scoped lang="scss">
-
+    .layout {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+    }
+    .hasSider{
+        flex-direction: row;
+    }
 </style>
