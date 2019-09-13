c32a90933501afe6ce2a87b20381cd28962bfa88
许勇辉 <564146958@qq.com>
2019-09-13 23:20:40 +0800

    toast实现多行文字支持

--- src/toast.vue
+++ src/toast.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="toast">
-        <slot></slot>
-        <div class="line"></div>
-        <div v-if="closeButton" @click="onClickClose">{{closeButton.text}}</div>
+    <div class="toast" ref="wrapper">
+<!--        <slot></slot>-->
+        <div v-html="$slots.default[0]" class="text"></div>
+        <div class="line" ref="line"></div>
+        <div v-if="closeButton" @click="onClickClose" class="close">{{closeButton.text}}</div>
     </div>
 </template>
 
@@ -16,16 +17,14 @@
             },
             autoCloseDelay: {
                 type: Number,
-                default: 10
+                default: 50
             },
             closeButton:{
                 type:Object,
                 default:()=>{
                     return{
                         text:'关闭',
-                        callback(){
-                            console.log('知道了')
-                        }
+                        callback:undefined
                     }
                 }
             }
@@ -36,6 +35,11 @@
                     this.close()
                 }, this.autoCloseDelay * 1000)
             }
+            this.$nextTick(()=>{
+                console.log(this.$refs.wrapper.getBoundingClientRect());
+                this.$refs.line.style.height=
+                    `${this.$refs.wrapper.getBoundingClientRect().height}px`
+            })
         },
         methods:{
             close(){
@@ -44,7 +48,10 @@
             },
             onClickClose(){
                 this.close();
-                this.closeButton.callback();
+                //如果传过来有button并且是一个函数
+                if(this.closeButton && typeof this.closeButton==='function'){
+                    this.closeButton.callback();
+                }
             }
         }
     }
@@ -52,11 +59,12 @@
 
 <style scoped lang="scss">
     $font-size:14px;
-    $toast-height:40px;
+    $toast-min-height:40px;
     $toast-bg:rgba(0,0,0,0.75);
     .toast {
         font-size:$font-size;
-        height:$toast-height;
+        line-height: 1.8;
+        min-height:$toast-min-height;
         background-color:$toast-bg;
         color: #fff;
         align-items: center;
@@ -75,6 +83,18 @@
         background-color: #666666;
         height: 100%;
     }
+    .close{
+        padding-left: 8px;
+        flex-shrink: 0;
+    }
+    .text{
+        margin: 8px 0;
+        max-width: 200px;
+        word-wrap: break-word;
+        word-break: break-all;
+        overflow: hidden;
+    }
+
 
 </style>
 
