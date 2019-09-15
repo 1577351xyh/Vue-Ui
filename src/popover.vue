0422017d2322d339d25f9729a6fcd6760a708092
许勇辉 <564146958@qq.com>
2019-09-15 15:27:57 +0800

    popover点击事件冒泡处理

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="popover" @click.stop="clickShow">
-        <div ref="contentWrapper" v-show="visible" class="content-wrapper" @click.stop="">
+        <div ref="contentWrapper" v-show="visible" class="content-wrapper" >
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -18,23 +18,31 @@ export default {
         }
     },
     methods:{
-        clickShow(){
-            console.log(111)
-            this.visible = !this.visible;
-            if(this.visible===true){
-                this.$nextTick(()=>{
-                    document.body.appendChild(this.$refs.contentWrapper);
-                    let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect();
-
-                    let PopoverClick= ()=> {
-                        this.visible=false;
-                        //删除ducoment监听
-                        document.removeEventListener('click',PopoverClick)
-                    };
-                    //新建监听
-                    document.addEventListener('click',PopoverClick)
-                });
+        clickShow(event){
+            //如果点击的事件中包含triggerWrapper
+            if(this.$refs.triggerWrapper.contains(event.target)){
+                this.visible = !this.visible;
+                if(this.visible===true){
+                    this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper);
+                        let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                        this.$refs.contentWrapper.style.left=left+window.scrollX+'px';
+                        this.$refs.contentWrapper.style.top=top+ window.scrollY+'px';
+                        let PopoverClick= (e)=> {
+                            //如果点击中包含e.target
+                            if(this.$refs.contentWrapper.contains(e.target)){
+                            }else{
+                                this.visible=false;
+                                //删除ducoment监听
+                                document.removeEventListener('click',PopoverClick)
+                            }
+                        };
+                        //新建监听
+                        document.addEventListener('click',PopoverClick)
+                    });
+                }
             }
+
         }
     },
     mounted(){
@@ -51,9 +59,8 @@ export default {
 }
 .content-wrapper{
     position: absolute;
-    bottom: 100%;
-    left: 0;
     border: 1px solid red;
     box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    transform: translateY(-100%);
 }
 </style>
