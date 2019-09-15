759f7de2768ca6c325ee56ed10d700da4c4e48c8
许勇辉 <564146958@qq.com>
2019-09-15 16:01:53 +0800

    popover优化

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click.stop="clickShow">
+    <div class="popover" @click.stop="clickShow" ref="popover">
         <div ref="contentWrapper" v-show="visible" class="content-wrapper" >
             <slot name="content"></slot>
         </div>
@@ -18,31 +18,40 @@ export default {
         }
     },
     methods:{
+        positionContent(){
+            document.body.appendChild(this.$refs.contentWrapper);
+            let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect();
+            this.$refs.contentWrapper.style.left=left+window.scrollX+'px';
+            this.$refs.contentWrapper.style.top=top+ window.scrollY+'px';
+        },
+        document(e){
+                //如果点击中包含e.target
+                if(this.$refs.contentWrapper.contains(e.target) || this.$refs.popover===e.target){
+                    return;
+                }else{
+                   this.close()
+                }
+        },
+        onOpen(){
+            this.visible = true;
+            this.$nextTick(()=>{
+                this.positionContent();
+                document.addEventListener('click',this.document)
+            });
+        },
+        close(){
+          this.visible =false;
+          document.removeEventListener('click',this.document)
+        },
         clickShow(event){
             //如果点击的事件中包含triggerWrapper
             if(this.$refs.triggerWrapper.contains(event.target)){
-                this.visible = !this.visible;
                 if(this.visible===true){
-                    this.$nextTick(()=>{
-                        document.body.appendChild(this.$refs.contentWrapper);
-                        let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect();
-                        this.$refs.contentWrapper.style.left=left+window.scrollX+'px';
-                        this.$refs.contentWrapper.style.top=top+ window.scrollY+'px';
-                        let PopoverClick= (e)=> {
-                            //如果点击中包含e.target
-                            if(this.$refs.contentWrapper.contains(e.target)){
-                            }else{
-                                this.visible=false;
-                                //删除ducoment监听
-                                document.removeEventListener('click',PopoverClick)
-                            }
-                        };
-                        //新建监听
-                        document.addEventListener('click',PopoverClick)
-                    });
+                    this.close();
+                }else {
+                    this.onOpen()
                 }
             }
-
         }
     },
     mounted(){
