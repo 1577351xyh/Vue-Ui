288beae95b03effa8273173b6f4a205ae723974f
许勇辉 <564146958@qq.com>
2019-09-26 00:14:44 +0800

    解决tab没有默认样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="tabs-header">
-
+        <slot></slot>
+        <div class="actions-wrapper">
+            <slot name="actions"></slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: ""
+        name: "",
+        inject:['eventBus'],
+        data(){
+            return{
+
+            }
+        }
     }
 </script>
 
-<style scoped lang="less">
+<style scoped lang="scss">
+    $height:40px;
 .tabs-header{
-
+    display: flex;
+    min-height: $height;
+    justify-content: flex-start;
+    align-items: center;
+    .actions-wrapper{
+        margin-left: auto;
+    }
 }
 </style>
