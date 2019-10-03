5145d4e3f376124fa43c43be80ec824fd63c7b0e
许勇辉 <564146958@qq.com>
2019-10-03 19:56:41 +0800

    触发后给childer,item添加事件,

--- src/tabs.vue
+++ src/tabs.vue
@@ -5,8 +5,49 @@
 </template>
 
 <script>
+    import Vue from 'vue'
     export default {
-        name: ""
+        name: "",
+        props:{
+            selected:{
+                type:String,
+                required:true
+            },
+            direction:{
+                type:String,
+                default:'horizontal',
+                validator(value){
+                    return ['horizontal','vertical'].indexOf(value)>=0
+                }
+            }
+        },
+        data(){
+            return{
+                eventBus:new Vue()
+            }
+        },
+        provide(){
+            return{
+                eventBus:this.eventBus
+            }
+        },
+        created(){
+            console.log(this.eventBus);
+        },
+        mounted() {
+            //打印子元素
+            this.$children.forEach((vm)=>{
+                if(vm.$options.name==='Head'){
+                    vm.$children.forEach((item)=>{
+                        if(item.$options.name==='TabItem' &&item.name===this.selected){
+                            this.eventBus.$emit('update:selected',this.selected,item)
+
+                        }
+                    })
+                }
+            })
+
+        }
     }
 </script>
 
