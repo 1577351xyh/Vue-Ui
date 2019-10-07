00dbf28d277a5236a4615c8430bb16c2d78c65d3
许勇辉 <564146958@qq.com>
2019-10-07 20:07:13 +0800

    实现手风琴效果,单次点击只出现一个内容,其余关闭

---
+++ src/collapse.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="collapse">
+    <slot>
+
+    </slot>
+</div>
+</template>
+
+<script>
+    import Vue from 'vue'
+export default {
+    props:{
+        single:{
+            type:Boolean,
+            default:false
+        }
+    },
+    data(){
+        return{
+            eventBus:new Vue()
+        }
+    },
+    provide(){
+
+            return{
+                eventBus: this.eventBus
+            }
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.collapse{
+    border: 1px solid gray;
+    border-radius: 8px;
+    overflow: hidden;
+}
+</style>
