29e35a134b29ae2eea5d36c91c8d7621497c2d75
许勇辉 <564146958@qq.com>
2019-10-07 16:31:18 +0800

    tab head动画

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="tabs-header">
         <slot></slot>
+        <div class="line" ref="line"></div>
         <div class="actions-wrapper">
             <slot name="actions"></slot>
         </div>
@@ -9,12 +10,20 @@
 
 <script>
     export default {
-        name: "",
+        name: "Head",
         inject:['eventBus'],
         data(){
             return{
 
             }
+        },
+        mounted(){
+            this.eventBus.$on('update:selected',(item,vm)=>{
+                let {width,height,top,left} = vm.$el.getBoundingClientRect();
+                console.log(left)
+                this.$refs.line.style.width=`${width}px`;
+                this.$refs.line.style.left = `${left}px`
+            })
         }
     }
 </script>
@@ -26,6 +35,13 @@
     min-height: $height;
     justify-content: flex-start;
     align-items: center;
+    position:relative;
+    .line{
+        position: absolute;
+        bottom: 0;
+        border-bottom: 2px solid blue;
+        transition: all 500ms;
+    }
     .actions-wrapper{
         margin-left: auto;
     }
