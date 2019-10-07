8883e0b8bf9c8ce913c5b40b1d0b4ae4689aa3c3
许勇辉 <564146958@qq.com>
2019-10-07 16:48:18 +0800

    tab样式优化,实现disable禁用

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,15 +1,67 @@
 <template>
-    <div class="tabs-item">
-
+    <div class="tabs-item" @click="onClick" :class="classes">
+        <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: ""
+        name: "TabItem",
+        inject:['eventBus'],
+        props:{
+            disabled:{
+                type:Boolean,
+                default:false
+            },
+            name:{
+                type:String|Number,
+                required:true
+            }
+        },
+        data(){
+            return{
+                active:false,
+            }
+        },
+        methods:{
+            onClick(){
+                if(this.disabled){return}
+                this.eventBus.$emit('update:selected',this.name,this)
+            }
+        },
+        created(){
+            this.eventBus.$on('update:selected',(name)=>{
+                if(name===this.name){
+                    this.active=true;
+                }else {
+                    this.active=false;
+                }
+            })
+        },
+        computed:{
+            classes(){
+                return {
+                    active:this.active,
+                    disabled:this.disabled
+                }
+            }
+        }
     }
 </script>
 
 <style scoped lang="less">
-
+.tabs-item{
+    cursor: pointer;
+    padding: 0 1em;
+    height:100%;
+    display:flex;
+    align-content: center;
+    &.active{
+       color: blue;
+    }
+    &.disabled{
+        color: #bbbbbb;
+        cursor: not-allowed;
+      }
+}
 </style>
