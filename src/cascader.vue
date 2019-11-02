14724f734b0014e906c3302cced6ae552b8201fe
许勇辉 <564146958@qq.com>
2019-11-02 14:47:52 +0800

    递归组件渲染基本实现

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,13 +1,41 @@
 <template>
-$END$
+    <div class="cascader">
+        <div class="trigger" @click="popoverVisible=true">
+            <slot></slot>
+        </div>
+        <div class="popover" v-if="popoverVisible">
+            <cascader-item :items="source"></cascader-item>
+        </div>
+    </div>
+
 </template>
 
 <script>
+    import cascaderItem from './cascader-item'
 export default {
-name: ""
-}
+        name: "",
+        components:{
+            cascaderItem
+        },
+        data(){
+            return{
+                popoverVisible:false
+            }
+        },
+        props:{
+            source:{
+                type:Array
+            }
+        }
+    }
+
 </script>
 
 <style scoped>
+    .trigger{
+        width:200px;
+        height: 100px;
+        border: 1px solid red;
+    }
 
 </style>
