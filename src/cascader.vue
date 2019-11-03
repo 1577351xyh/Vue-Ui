b4e316257b551c7457b675ba206e971cf3e61338
许勇辉 <564146958@qq.com>
2019-11-03 18:40:50 +0800

    完成基本样式

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="cascader">
         <div class="trigger" @click="popoverVisible=true">
-            <slot></slot>
+            {{result || '请选择省市'}}
         </div>
-        <div class="popover" v-if="popoverVisible">
-            <cascader-item :items="source"></cascader-item>
+        <div class="popover" :style="{height:itemheight}" v-if="popoverVisible">
+            <cascader-item :selected="selected" @update:selected="onUpdateSelected" :items="source"></cascader-item>
         </div>
     </div>
 
@@ -25,17 +25,51 @@ export default {
         props:{
             source:{
                 type:Array
+            },
+            itemheight:{
+                type:String
+            },
+            selected:{
+                type:Array,
+                default:()=>{
+                    return [];
+                }
             }
+        },
+    methods:{
+        onUpdateSelected(newSelected){
+            this.$emit('update:selected',newSelected)
         }
+    },
+    computed:{
+            //把当前被选中的item.name做字符串拼接返回一个字符在串
+        result(){
+            return this.selected.map((item)=>item.name).join('/')
+        }
+    }
     }
 
 </script>
 
 <style scoped>
+    .cascader{
+        position: relative;
+    }
     .trigger{
-        width:200px;
-        height: 100px;
-        border: 1px solid red;
+        min-width:10em;
+        display: inline-flex;
+        align-items: center;
+        padding: 0 1em;
+        height: 30px;
+        border-radius: 4px;
+        border: 1px solid #999999;
+    }
+    .popover{
+        position: absolute;
+        top: 100%;
+        left: 0;
+        z-index: 1;
+        background: #ffffff;
     }
 
 </style>
