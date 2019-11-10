2e43b554b334c3a914c9c802a5ebaf098fb7a3f5
许勇辉 <564146958@qq.com>
2019-11-10 18:54:26 +0800

    tab组件完成

---
+++ src/demo.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <cascader :selected="selected" @update:selected="selected = $event" :source="cascader" :itemheight="'200px'"></cascader>
+
+    </div>
+</template>
+
+<script>
+import cascader from './cascader'
+export default {
+    name: "",
+    data(){
+        return{
+            selected:[],
+            cascader:[
+                {
+                    name:'汉洲',
+                    children:[
+                        {name:'上城'},
+                        {name:'上城2'},
+                        {name:'上城3'}
+                    ]
+                },
+                {
+                    name:'嘉兴',
+                    children:[
+                        {name:'南湖'},
+                        {name:'南湖1'},
+                        {name:'南湖2'}
+                    ]
+                },
+                {
+                    name:'福建',
+                    children:[
+                        {
+                            name:'福州',
+                            children:[
+                                {name:'台江'},
+                                {name:'仓山'}
+                            ]
+                        },
+
+                    ]
+                },
+            ]
+        }
+    },
+    components:{
+        cascader,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+import demo from './demo'
+
+new Vue({
+    render: h => h(demo),
+}).$mount('#app')
