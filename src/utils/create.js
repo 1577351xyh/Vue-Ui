3f5f6d4b1c9a9f2ebe239d016c04fab33fea74af
1577351xyh <564146958@qq.com>
2019-12-06 00:18:18 +0800

    toast-2 虚拟dom创建

---
+++ src/utils/create.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+export default function create(Component,props) {
+    //创建一个独立的vue实例
+    const vm = new Vue({
+        render(h){
+            //h是createElment的简写,创建虚拟dom
+            return h(Component,{props})
+        }
+    }).$mount();//挂载
+    
+    //手动挂载
+    document.body.appendChild(vm.$el);
+
+    //销毁
+    const comp = vm.$children[0];
+    comp.remove = function (){
+        ducoment.body.removeChild(vm.$el);
+        vm.$destroy();//销毁
+    };
+    return comp;
+}
