473517ddddd987ddb28f788297886582b164293c
1577351xyh <564146958@qq.com>
2020-05-06 22:36:35 +0800

    feat:date-picker curren日期高亮

--- src/datePicker/date-picker.vue
+++ src/datePicker/date-picker.vue
@@ -3,16 +3,40 @@
     <g-popover position="bottom">
       <g-input class="input__inner" readonly="" placeholder="请选择日期" />
       <template slot="content">
-        <div class="date-pick-nav"></div>
-        <div class="date-pick-week"></div>
+        <div class="date-pick-nav">
+          <span><g-icon name="zuozuo"></g-icon></span>
+          <span><g-icon name="left"></g-icon></span>
+          <span>2018年</span>
+          <span>5月</span>
+          <span><g-icon name="youyou"></g-icon></span>
+          <span><g-icon name="right"></g-icon></span>
+        </div>
+        <div class="dete-pick-line"></div>
+        <div class="date-pick-week">
+          <span v-for="(i, index) in [1, 2, 3, 4, 5, 6, 0]" :key="index">
+            {{ weekdats[i] }}
+          </span>
+        </div>
         <div class="date-pick-content">
-          <div v-for="(i, index) in helper.range(1, 7)" :key="index">
-            <span v-for="(j, index) in helper.range(1, 8)" :key="index">{{
-              visibleDays[(i - 1) * 7 + j - 1].getDate()
-            }}</span>
+          <div
+            class="date-pick-row"
+            v-for="(i, index) in helper.range(1, 7)"
+            :key="index"
+          >
+            <span
+              :class="[
+                'date-pick-col',
+                { 'date-pcik-cell': isCurrentMonth(getVisibleDay(i, j)) },
+              ]"
+              v-for="(j, index) in helper.range(1, 8)"
+              :key="index"
+              >{{ visibleDays[(i - 1) * 7 + j - 1].getDate() }}</span
+            >
           </div>
         </div>
-        <div class="date-pick-actions"></div>
+        <div class="date-pick-actions">
+          <button>清除</button>
+        </div>
         <div></div>
       </template>
     </g-popover>
@@ -21,18 +45,38 @@
 
 <script>
 import GInput from '@/input.vue'
+import GIcon from '@/icon/icon.vue'
 import GPopover from '@/popover/popover.vue'
-import helper from './helper'
+import helper from './helper.js'
 export default {
-  components: { GInput, GPopover },
+  components: { GInput, GPopover, GIcon },
   data() {
     return {
       value: new Date(),
       helper,
+      weekdats: ['日', '一', '二', '三', '四', '五', '六'],
+      display: {
+        year: null,
+        month: null,
+      },
     }
   },
   mounted() {
-    console.log(this.visibleDays)
+    console.log()
+  },
+  created() {
+    this.display.year = helper.getYearMonthDate(this.value)[0]
+    this.display.month = helper.getYearMonthDate(this.value)[1]
+  },
+  methods: {
+    isCell(i, j) {},
+    getVisibleDay(row, col) {
+      return this.visibleDays[(row - 1) * 7 + col - 1]
+    },
+    isCurrentMonth(date) {
+      let [year1, month1] = helper.getYearMonthDate(date)
+      return year1 === this.display.year && month1 === this.display.month
+    },
   },
   computed: {
     visibleDays() {
@@ -74,5 +118,35 @@ export default {
       cursor: pointer !important;
     }
   }
+}
+.date-pick-week,
+.date-pick-nav,
+.date-pick-row {
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 0px;
+  padding: 0 5px;
+  cursor: pointer;
+  .date-pick-col {
+    margin: 0px 5px;
+    width: 22px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    color: #bbb;
+  }
+  .date-pcik-cell {
+    color: #000;
+  }
+}
+
+.date-pick-row {
+  padding: 0;
+}
+.dete-pick-line {
+  height: 1px;
+  width: 100%;
+  background-color: #bbb;
 }
 </style>
